db164a96a2b61e3dcdb44f233965072490b93388
Silas Michael Knight <silasfelinus@gmail.com>
2021-03-10 03:07:21 +0000

    Automatically backed up by Learn

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,16 @@
+# db/migrate/01_create_artists.rb
+
+class CreateArtists < ActiveRecord::Migration[5.2]
+  def up
+  end
+
+  def down
+  end
+end
+
+
+class CreateArtists < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
+ 
